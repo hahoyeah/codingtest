25c88e73d97a245efbd80cffb3d148b518c39cfa
yejin <80815939+hahoyeah@users.noreply.github.com>
2023-05-09 19:16:04 +0900

    [level 1] Title: 행렬의 덧셈, Time: 38.41 ms, Memory: 22.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -6,6 +6,14 @@ def solution(arr1, arr2):
         for j in range(len(arr1[i])):
             s.append(arr1[i][j] + arr2[i][j])
         answer.append(s)    
-        
-        
     return answer
+    
+    #행과 열의 크기가 같기때문에 arr1[i]이렇게 굳이 안해도 돼
+#     for i in range(len(arr1)):
+#         for j in range(len(arr1[0])):
+#             arr1[i][j] += arr2[i][j]
+        
+#     return arr1
+
+    
+    
