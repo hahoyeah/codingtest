3a54522802b8adf747eb6d58d8c682e6020b4df5
yejin <80815939+hahoyeah@users.noreply.github.com>
2023-05-23 16:37:22 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,17 +1,30 @@
 def solution(s):
-    k = s.split()    
-    ls= list(s)
-    answer = []
-    for i in k:
-        for j,v in enumerate(i):
-            if j == 0:
-                answer.append(v.upper())
-            elif j % 2 == 0:
-                answer.append(v.upper())
+    a=[]
+    s=s.split(" ")
+    for i in range(len(s)):
+        for j in range(len(s[i])):
+            if j%2==0:
+                a.append(s[i][j].upper())
             else:
-                answer.append(v.lower())
-    for m in range(len(ls)):
-        if ls[m] == " ":
-            answer.insert(m," ") #insert는 리스트에
+                a.append(s[i][j].lower())
+        a.append(" ")
+
+    c="".join(a[:-1])
+    return c
+
+#     k = s.split()    
+#     ls= list(s)
+#     answer = []
+#     for i in k:
+#         for j,v in enumerate(i):
+#             if j == 0:
+#                 answer.append(v.upper())
+#             elif j % 2 == 0:
+#                 answer.append(v.upper())
+#             else:
+#                 answer.append(v.lower())
+#     for m in range(len(ls)):
+#         if ls[m] == " ":
+#             answer.insert(m," ") #insert는 리스트에
             
-    return "".join(answer)
+#     return "".join(answer)
