1124321b54ec5a48ac06cf46ec2f91ae33cd672d
yejin <80815939+hahoyeah@users.noreply.github.com>
2023-05-29 00:13:26 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.py
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -1,9 +1,7 @@
 def solution(array, commands):
     answer = []
     for v in range(len(commands)):
-        i,j,k = commands[v][0], commands[v][1], commands[v][2]
-        a = array[i-1:j]
-        a.sort()
-        answer.append(a[k-1])
+        i,j,k = commands[v]
+        answer.append(sorted(array[i-1:j])[k-1])
     return answer
         
