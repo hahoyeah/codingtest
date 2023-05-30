c5de16f709170b184c252a30091c1cbb729e8cc6
yejin <80815939+hahoyeah@users.noreply.github.com>
2023-05-30 20:15:14 +0900

    [unrated] Title: 추억 점수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
@@ -1,8 +1,8 @@
 def solution(name, yearning, photo):
     answer = [0] * len(photo)
-    dic = dict()
-    for i,j in zip(name, yearning):
-        dic[i]=j
+    dic = dict(zip(name, yearning))
+    # for i,j in zip(name, yearning):
+    #     dic[i]=j
     for k in range(len(photo)):
         for v in photo[k]:
             if v in dic:
