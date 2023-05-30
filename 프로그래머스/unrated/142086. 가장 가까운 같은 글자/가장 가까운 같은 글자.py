2f3697da6dce56a01df908b67f2cef2777705804
yejin <80815939+hahoyeah@users.noreply.github.com>
2023-05-30 16:19:12 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 3034.44 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -1,4 +1,14 @@
 def solution(s):
+    # dic = dict()
+    # answer=[]
+    # for i,v in enumerate(s):
+    #     if v not in dic:
+    #         answer.append(-1)
+    #     else:
+    #         answer.append(i-dic[v])
+    #     dic[v]=i
+    # return answer
+
     p = [] #처음인것들
     answer = [0]*len(s)
     for i,v in enumerate(s):
