a37ec20b56635a2a4dcf95795d1f4468401edb72
yejin <80815939+hahoyeah@users.noreply.github.com>
2023-06-01 22:34:13 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -9,3 +9,12 @@ def solution(k, score):
             ks[0] = i
         answer.append(min(ks))
     return answer
+    
+    # ks = []
+    # answer = []
+    # for i in score:
+    #     ks.append(i)
+    #     if len(ks)>k:
+    #         ks.remove(min(ks)) #remove
+    #     answer.append(min(ks))
+    # return answer
