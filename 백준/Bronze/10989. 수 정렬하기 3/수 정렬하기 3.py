f279d3a8dad081ecc27c7068207a6ce711a97f17
yejin <80815939+hahoyeah@users.noreply.github.com>
2023-06-17 01:58:14 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 8756 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,12 +1,11 @@
 import sys
 input = sys.stdin.readline
 
-n= int(input())
-count = [0]*(10_000+1) #0도 자연수에 포함하기 때문에 1을 더해주는 듯?(문의)
+n = int(input())
+count = [0]*(10000+1)
 for _ in range(n):
     count[int(input())] += 1
-    
+
 for i in range(len(count)):
-    for j in range(count[i]):
-        print(i) #오름차순으로, count의 인덱스는 수, 값은 개수
-        
+    for _ in range(count[i]):
+        print(i)
