83705956efb6c56d061cfe54688facf04f7682be
yejin <80815939+hahoyeah@users.noreply.github.com>
2023-06-23 21:37:34 +0900

    [level 1] Title: 소수 찾기, Time: 535.99 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
@@ -10,10 +10,11 @@
 
 
 def solution(n):
-    num=set(range(2,n+1))
+    # set은 중복제거 및 순서 정렬
+    num = set(range(2,n+1))
     
-    for i in range(2,n):
+    for i in range(2,n+1):
         if i in num:
-            num-=set(range(2*i,n+1,i))
+            num -= set(range(i*2, n+1, i))
             
     return len(num)
