f3d25ff660cf41ebb53871f674ddee55c2040e7f
yejin <80815939+hahoyeah@users.noreply.github.com>
2023-06-23 20:14:13 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
@@ -1,12 +1,13 @@
 import itertools
-#from itertools import combinations as cb
+from itertools import combinations as cb #cb(n,k)=itertools.combinations(n,k)
 # itertools.combinations(n,k)은 순서상관없이 n개중에서 서로다른 k개 선택(조합)
 # permutations은 순서상관있음!(순열)
+
 def solution(nums):
     result = len(list(itertools.combinations(nums,3)))
-    for i in list(itertools.combinations(nums,3)):
+    for i in list(cb(nums,3)):
         s = sum(i)
-        for v in range(2,s):
+        for v in range(2,s): #int(s**0.5) + 1
             if s % v == 0:
                 result -= 1
                 break
