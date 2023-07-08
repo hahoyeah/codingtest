bdf8dc2b73825ac4bf9774bc4bda5656c71af985
yejin <80815939+hahoyeah@users.noreply.github.com>
2023-07-09 00:32:36 +0900

    [level 2] Title: 구명보트, Time: 8.79 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.py
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -1,19 +1,16 @@
 def solution(people, limit):
-    #정렬한 뒤에 왼쪽 오른쪽을 더해서
-    #limit보다 크면 맨 오른쪽은 혼자태움
-    #그 다음으로 덜뚱뚱한 사람으로 비교
-    result = 0
     people.sort()
     left = 0
-    right = len(people) - 1
-    while left <= right:
-        if people[left] + people[right] > limit:
-            result += 1
+    right = len(people)-1
+    result = 0
+    
+    while left<=right:
+        if people[left]+people[right]>limit:
             right -= 1
-        else:
             result += 1
+        else:
             left += 1
             right -= 1
-    return result
+            result += 1
             
-        
+    return result
