ee33e3bca4e8878f6e66f3fd7761742c7f352cdf
yejin <80815939+hahoyeah@users.noreply.github.com>
2023-07-08 21:02:59 +0900

    [unrated] Title: 대충 만든 자판, Time: 0.73 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.py
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.py
@@ -3,15 +3,18 @@ def solution(keymap, targets):
     dic = defaultdict(int)
     result = [0]*len(targets)
     
-    for key in keymap:
-        for i in range(len(key)):
-            if dic[key[i]] > i+1 or dic[key[i]]==0:
-                dic[key[i]] = i+1
-    for t in range(len(targets)):
-        for k in targets[t]:
-            if dic[k] == 0:
-                result[t] = -1
+    for i in keymap:
+        for ind, key in enumerate(i):
+            ind = ind+1
+            if key in dic and dic[key]>ind:
+                dic[key] = ind
+            if key not in dic:
+                dic[key] = ind
+    
+    for k in range(len(targets)):
+        for key in targets[k]:
+            result[k] += dic[key]
+            if dic[key] == 0:
+                result[k] = -1
                 break
-            else:
-                result[t] += dic[k]
     return result
