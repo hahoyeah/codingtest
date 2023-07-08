f80f7d5f0821f73a05eca1dbd687dde3c9b61d7b
yejin <80815939+hahoyeah@users.noreply.github.com>
2023-07-08 19:08:01 +0900

    [unrated] Title: 정수를 나선형으로 배치하기, Time: 0.28 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
+++ 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
@@ -1,48 +1,20 @@
 def solution(n):
-    #달팽이구조
+    d = [[0]*n for _ in range(n)]
+    d[0][0]=1
+    L=1
+    dx = [-1,0,1,0]
+    dy = [0,1,0,-1]
     r,c = 0,0
-    d = 1
-    dr = [-1,0,1,0]
-    dc = [0,1,0,-1]
-    end = n*n
-    cnt=1
-    graph = [[0] * n for _ in range(n)]
-    graph[0][0]=1
-    while cnt < end:
-        nr = r + dr[d]
-        nc = c + dc[d]
-        if 0<=nr<n and 0<=nc<n and graph[nr][nc] == 0:
-            #graph[nr][nc] == 0이게 뒤에 가야한다.
-            cnt += 1
-            r = nr
-            c = nc
-            graph[r][c] = cnt
+    i = 1
+    
+    while L<n*n:
+        nr = r+dx[i]
+        nc = c+dy[i]
+        if 0<=nr<n and 0<=nc<n and d[nr][nc]==0:
+            L+=1
+            d[nr][nc] = L
+            r,c = nr,nc
         else:
-            d = (d+1)%4
-    return graph
-
-#def solution(n):
-#    #달팽이구조
-#    r,c = 0,0
-#    d = 1
-#    dr = [-1,0,1,0]
-#    dc = [0,1,0,-1]
-#    end = n*n
-#    cnt=1
-#    graph = [[0] * n for _ in range(n)]
-#
-#    while cnt < end:
-#        nr = r + dr[d]
-#        nc = c + dc[d]
-#        if nr<0 or nr>=n or 0>nc or nc>=n or graph[nr][nc] != 0:
-#            d = (d+1)%4
-#            continue
-#        graph[r][c] = cnt
-#        cnt += 1
-#        r = nr
-#        c= nc
-#    graph[r][c] = cnt    
-#        
-#    return graph
-
-#print(solution(4))
+            i = (i+1)%4
+    return d
+            
