4af3d5219f01a4d4c28b75dc1e04e427fcdd5db5
yejin <80815939+hahoyeah@users.noreply.github.com>
2023-07-08 20:09:48 +0900

    [unrated] Title: 공원 산책, Time: 0.10 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/unrated/172928. 공원 산책/공원 산책.py
+++ 프로그래머스/unrated/172928. 공원 산책/공원 산책.py
@@ -1,28 +1,33 @@
 def solution(park, routes):
-    #split() 이용
-    dr = [-1,0,1,0]
-    dc = [0,1,0,-1]
-    d = {'N':0, 'S':2, 'W':3, 'E':1}
     n = len(park)
     m = len(park[0])
-    r,c = 0,0
-        
-    for i in range(n): # 행
-        for j in range(m): # 열
-            if park[i][j] == "S":
-                r,c = i,j
-               
-    for k in routes:
-        a,b = k.split()
-        b = int(b)
-        lr,lc = r,c 
-        for _ in range(b):
-            nr = r + dr[d[a]]
-            nc = c + dc[d[a]]
-            if nr<0 or nr>=n or nc<0 or nc>=m or park[nr][nc]=="X":
-                r,c = lr,lc
-                break
-            r = nr
-            c = nc
+    dic = {
+        'N':(-1,0),
+        'S':(1,0),
+        'W':(0,-1),
+        'E':(0,1)
+    }
     
-    return [r,c]
+    for i in range(n):
+        for j in range(m):
+            if park[i][j] == 'S':
+                r,c = i,j
+    kr,kc = r,c            
+    for k in routes:
+        op, count = k.split()
+        for _ in range(int(count)):
+            nr = kr + dic[op][0]
+            nc = kc + dic[op][1]
+            if 0<=nr<n and 0<=nc<m and park[nr][nc]!='X':
+                kr,kc = nr,nc
+            else:
+                kr,kc = r,c
+                break
+        r,c = kr,kc
+    
+    return r,c
+
+        
+        
+    
+    
