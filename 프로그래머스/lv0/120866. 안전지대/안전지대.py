701b3b5dba29be824a64f5f8707f16a8dc3c36ef
yejin <80815939+hahoyeah@users.noreply.github.com>
2023-07-08 17:21:53 +0900

    [level 0] Title: 안전지대, Time: 0.12 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.py
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.py
@@ -1,19 +1,20 @@
 def solution(board):
-    dr = [-1,-1,0,1,1,1,0,-1]
-    dc = [0,1,1,1,0,-1,-1,-1]
+    dx = [-1,-1,0,1,1,1,0,-1]
+    dy = [0,1,1,1,0,-1,-1,-1]
+    result = 0
     n = len(board)
-    answer = n*n
-    count = 0
     for i in range(n):
         for j in range(n):
             if board[i][j] == 1:
-                count += 1
-                for k in range(8):
-                    nr = dr[k] + i
-                    nc = dc[k] + j
-                    if 0<=nr<n and 0<=nc<n and board[nr][nc]==0:
-                        count += 1
-                        board[nr][nc] = 2 
-    return answer - count
-            
-            
+                for k in range(len(dx)):
+                    nx = i+dx[k]
+                    ny = j+dy[k]
+                    if 0<=nx<n and 0<=ny<n and board[nx][ny]==0:
+                        board[nx][ny] = 2
+    
+    for i in range(n):
+        for j in range(n):
+            if board[i][j]==0:
+                result += 1
+    
+    return result
