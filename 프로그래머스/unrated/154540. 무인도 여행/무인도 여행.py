f8c486f3a0e2dc52863e0a598cdec0139f7badb1
yejin <80815939+hahoyeah@users.noreply.github.com>
2023-07-09 12:26:54 +0900

    [unrated] Title: 무인도 여행, Time: 0.30 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.py
+++ 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.py
@@ -1,31 +1,29 @@
-#런타임 에러를 안나게 해준다.
-# 즉 깊이 파고들 수 있게끔 도와준다
-# 런타임 에러를 안나게 해주는 스킬!
 import sys
-sys.setrecursionlimit(int(1e6)) #1e9
+sys.setrecursionlimit(int(1e6))
 
 def solution(maps):
-    #지도, 집개수 이런건 무조건 dfs
-    result = []
+    dx = [-1,0,1,0]
+    dy = [0,1,0,-1]
     n = len(maps)
     m = len(maps[0])
-    v = [[0]*m for _ in range(n)]
+    visit = [[0]*m for _ in range(n)]
+    result = []
     
     def DFS(x,y):
-        if 0<=x<n and 0<=y<m and v[x][y]==0 and maps[x][y]!='X':
-            v[x][y] = 1
-            a = DFS(x+1,y)
-            b = DFS(x-1,y)
-            c = DFS(x,y+1)
-            d = DFS(x,y-1)
-            return a+ b+ c+ d+ int(maps[x][y])
+        s = 0
+        if 0<=x<n and 0<=y<m and maps[x][y]!='X' and visit[x][y]==0:
+            visit[x][y]=1
+            for i in range(4):
+                s += DFS(x+dx[i],y+dy[i])
+            return s + int(maps[x][y])
         else:
             return 0
         
     for i in range(n):
         for j in range(m):
             size = DFS(i,j)
-            if size > 0:
+            if size>0:
                 result.append(size)
     
-    return sorted(result) if len(result)>0 else [-1]
+    return sorted(result) if result else [-1]
+                
