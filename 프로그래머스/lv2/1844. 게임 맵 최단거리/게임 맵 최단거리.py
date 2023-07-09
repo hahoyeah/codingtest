85d234c18af986342c5d211af2351f8d9071c64c
yejin <80815939+hahoyeah@users.noreply.github.com>
2023-07-09 10:36:22 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.51 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,28 +1,35 @@
 def solution(maps):
     from collections import deque
-    dr = [-1,0,1,0]
-    dc = [0,1,0,-1]
     q = deque()
+    dx = [-1,0,1,0]
+    dy = [0,1,0,-1]
     n = len(maps)
     m = len(maps[0])
-    dist = [[0]*m for _ in range(n)]
-    L = 1
-    q.append([0,0])
-    maps[0][0] = 0
-    # 도착지점 n-1, m-1
+    visit = [[0]*m for _ in range(n)]
+    x,y = 0,0
+    visit[x][y]=1
+    #L=0
+    q.append((x,y))
+    
+    # while q:
+    #     L+=1
+    #     for _ in range(len(q)):
+    #         x,y = q.popleft()
+    #         for i in range(4):
+    #             nx = x+dx[i]
+    #             ny = y+dy[i]
+    #             if 0<=nx<n and 0<=ny<m and visit[nx][ny]==0 and maps[nx][ny]==1:
+    #                 x, y = nx, ny
+    #                 visit[nx][ny] = L+1    
+    
     while q:
-        l = len(q)
-        for _ in range(l):
-            r,c = q.popleft()
-            for i in range(4):
-                nr = r + dr[i]
-                nc = c + dc[i]
-                if 0<=nr<n and 0<=nc<m and maps[nr][nc] == 1:
-                    maps[nr][nc] = 0
-                    q.append([nr,nc])
-                    dist[nr][nc] = L+1
-        L+=1
-    return dist[n-1][m-1] if dist[n-1][m-1]!=0 else -1
-                    
-                    
-            
+        x,y = q.popleft()
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
+            if 0<=nx<n and 0<=ny<m and visit[nx][ny]==0 and maps[nx][ny]==1:
+                visit[nx][ny] = visit[x][y]+1
+                q.append((nx,ny))
+
+    return visit[n-1][m-1] if visit[n-1][m-1]!=0 else -1
+    
