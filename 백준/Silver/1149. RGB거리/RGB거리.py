ee43860fc42c0faea4b80df77def853439ac6376
yejin <80815939+hahoyeah@users.noreply.github.com>
2024-05-03 13:30:00 +0900

    [Silver I] Title: RGB거리, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -0,0 +1,17 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+n = int(input())
+graph = [list(map(int,input().split())) for _ in range(n)]
+
+d = [[0]*3 for _ in range(n)]
+
+d[0] = graph[0]
+
+for i in range(1,n):
+        d[i][0] = min(d[i-1][1],d[i-1][2])+graph[i][0]
+        d[i][1] = min(d[i-1][0],d[i-1][2])+graph[i][1]
+        d[i][2] = min(d[i-1][1],d[i-1][0])+graph[i][2]
+
+print(min(d[n-1]))
